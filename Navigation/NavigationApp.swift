47a4947fa585208fe219a9e755e28bb4d2056973
Nathan Fallet <contact@nathanfallet.me>
2022-07-07 13:37:54 +0200

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Nathan Fallet on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
